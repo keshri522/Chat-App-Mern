3e49cb24b0d98c22db37735a352c0d692e9a46b2
keshri522 <rkeshri522@gmail.com>
2023-05-19 21:53:04 +0530

    add some changes in databse connections

--- backend/configDb/dtabaseConnection.js
+++ backend/configDb/dtabaseConnection.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 // here just try to connect our Mongo db with backend..
 const ConnectDb = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
+    const ConnectionString="mongodb+srv://rkeshri522:Rahulkeshri123@cluster0.pmobmox.mongodb.net/ChatApplicationDb?retryWrites=true&w=majority"
+    const connection = await mongoose.connect(ConnectionString, {
       UseNewUrlParser: true,
       useUnifiedTopology: true,
       //   useFindAndModify: true,
