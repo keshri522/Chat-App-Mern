d85e69fb7577f3da00a4e1255bebb45796f53622
keshri522 <rkeshri522@gmail.com>
2023-05-19 15:57:27 +0530

    a

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import store from "./Redux/store";
 import { Provider } from "react-redux";
-
+import { HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </ChakraProvider>
 );
